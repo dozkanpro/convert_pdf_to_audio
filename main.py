b6ba6d821da2bb9c280f4ac3202bd9d8b9923883
Deniz <denizkozkan@hotmail.com>
2023-10-22 13:40:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+"""Synthesizes speech from the input string of text or ssml.
+Make sure to be working in a virtual environment.
+
+Note: ssml must be well-formed according to:
+    https://www.w3.org/TR/speech-synthesis/
+"""
+from google.cloud import texttospeech
+import PyPDF2
+
+# Convert .pdf to .txt
+
+with open('Hello Deniz.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+    text = ''
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+
+with open('output.txt', 'w') as text_file:
+    text_file.write(text)
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+with open('output.txt', 'r') as text_file:
+    text = text_file.read()
+
+synthesis_input = texttospeech.SynthesisInput(text=text)
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(
+    input=synthesis_input, voice=voice, audio_config=audio_config
+)
+
+# The response's audio_content is binary.
+with open("output.mp3", "wb") as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
